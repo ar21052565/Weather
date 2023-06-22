e73b8e2bb67fb23ce7fe56b40801df68bd8d2e3b
Ankit565 <97824339+ar21052565@users.noreply.github.com>
2023-06-23 02:07:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+let city = document.querySelector('.js-city');
+let temp = document.querySelector('.js-temp');
+let type = document.querySelector('.js-type');
+let image = document.querySelector('.js-img');
+let input = document.querySelector('.js-input');
+let API_Key = "c3ff8f1ed17bfccd4d387a9e5c00aca5";
+
+const data = async function (search) {
+    let getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_Key}&units=metric`);
+    let jsondata = await getData.json();
+    console.log(jsondata);
+
+    if (jsondata.cod == 400) {
+
+        image.src = "error1.png";
+        alert("Please Enter Location!");
+        city.innerHTML = "";
+        temp.innerHTML = "";
+        type.innerHTML = "";
+    }
+    if (jsondata.cod == 404) {
+
+        image.src = "error2.png";
+        document.querySelector('.js-img').classList('location');
+        city.innerHTML = search;
+        temp.innerHTML = "";
+        type.innerHTML = "";
+        alert("Please Enter Right Location!");
+    }
+
+    city.innerHTML = search;
+    temp.innerHTML = Math.floor(jsondata.main.temp) + "°C";
+    type.innerHTML = jsondata.weather[0].main;
+
+
+
+    if (type.innerHTML == "Clouds") {
+        image.src = "clouds.png";
+    }
+
+    else if (type.innerHTML == "Clear") {
+        image.src = "clears.png"
+    }
+
+    else if (type.innerHTML == "Haze") {
+        image.src = "haze.png"
+    }
+
+    else if (type.innerHTML == "Rain") {
+        image.src = "rain.png"
+    }
+
+    else if (type.innerHTML == "Strom") {
+        image.src = "strom.png"
+    }
+}
+
+function myFun() {
+    search = input.value;
+    data(search);
+    input.value = '';
+}
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        search = input.value;
+        data(search);
+        input.value = '';
+
+    }
+});
+
